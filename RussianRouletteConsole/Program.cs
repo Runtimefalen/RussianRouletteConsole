6488740d5a9994fe78eb971c84dbc4c6df98f296
alba <albert.khachaturov@inbox.ru>
2022-09-24 16:19:05 +0300

    update name2

--- RussianRouletteConsole/Program.cs
+++ RussianRouletteConsole/Program.cs
@@ -7,7 +7,7 @@ namespace RussianRoulette
         {
 
             ///see this text
-            var players = new List<string> { "Alfred", "Nicolas", "Jimmy", "Sammuel", "Richard" };
+            var players = new List<string> { "Alfred", "Nicky", "JiJimmy", "Sammuel", "Richard" };
             var gun = new Gun();
             int roundNumber = 1;
             
